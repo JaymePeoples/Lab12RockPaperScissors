df7ca26a96420de1a0553feff1775397b4c0df63
JaymePeoples <jaytrax@hotmail.com>
2017-07-12 06:00:38 -0400

    Commit 2

--- src/RoshamboApp.java
+++ src/RoshamboApp.java
@@ -7,8 +7,8 @@ public class RoshamboApp {
     public static void main( String[] args ) {
         //all classes will tie into this
         Scanner scnr = new Scanner(System.in);
-        String userName = "";
-        String userChoice = "";
+        String userName;
+        String userChoice;
 
         RockPlayer c1 = new RockPlayer();
         RandomPlayer c2 = new RandomPlayer();
@@ -20,29 +20,56 @@ public class RoshamboApp {
         player.setName(userName);
 
         do {
-            System.out.println("Who would you like to play against " + userName + "?" + "Please choose c1 or c2");
+            System.out.println("Who would you like to play against " + userName + "?" + " Please choose c1 or c2");
             userChoice = scnr.nextLine();
 
             Roshambo rockChoice = c1.generateRoshambo();
             Roshambo randomChoice = c2.generateRoshambo();
             Roshambo playerSelection = player.generateRoshambo();
 
-            if (userName.equalsIgnoreCase("c1")){
+            if (userChoice.equalsIgnoreCase("c1")) {
                 System.out.println("C1: " + rockChoice);
-                System.out.println(userName + ": " + playerSelection);
+                System.out.println(player.getName() + ": " + playerSelection);
 
-                if ()
+                if (playerSelection.equals(rockChoice)) {
+                    System.out.println("Draw");
+                } else if (playerSelection.toString().equalsIgnoreCase("Scissors")) {
+                    System.out.println(player.getName() + " " + "loses");
+                } else if (playerSelection.toString().equalsIgnoreCase("Paper")) {
+                    System.out.println(player.getName() + " " + "wins!");
+                    }
+                }
+                if (userChoice.equalsIgnoreCase("c2")) {
+                    System.out.println("C2 " + randomChoice);
+                    System.out.println(player.getName() + ": " + playerSelection);
+
+                    if (playerSelection.equals(randomChoice)) {
+                        System.out.println("Draw");
+                    } else if (playerSelection.toString().equalsIgnoreCase("Scissors")) {
+                        if (randomChoice.toString().equalsIgnoreCase("Rock")) {
+                            System.out.println(player.getName() + " loses");
+                        } else {
+                            System.out.println(player.getName() + " wins!");
+                        }
+                    } else if (playerSelection.toString().equalsIgnoreCase("Rock")) {
+                        if (randomChoice.toString().equalsIgnoreCase("Scissors")) {
+                            System.out.println(player.getName() + " wins!");
+                        } else {
+                            System.out.println(player.getName() + " loses");
+                        }
+                    } else if (playerSelection.toString().equalsIgnoreCase("Paper")) {
+                        if (randomChoice.toString().equalsIgnoreCase("Scissors")) {
+                            System.out.println(player.getName() + " loses");
+                        } else {
+                            System.out.println(player.getName() + " wins!");
+                        }
+                    }
+                }
+                System.out.println("Would you like to play again? (y/n)");
+                userChoice = scnr.nextLine();
             }
+            while (userChoice.equalsIgnoreCase("y")) ;
+            System.out.println(("Thanks for playing, " + player.getName() + "." + " Let's play again soon."));
         }
-
-        RockPlayer computer1 = new RockPlayer();
-        System.out.println("Computer 1 " + computer1.generateRoshambo().toString());
-
-        RandomPlayer computer2 = new RandomPlayer();
-        System.out.println("Computer 2 " + computer2.generateRoshambo().toString());
-
-
-    }
-
     }
 
